a6095f346a86dbcaae3ebfc5cdc2a14998f5f0c2
Deji <wrecodde@gmail.com>
2020-10-12 21:19:52 +0100

    use json responses

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import os
 
 import flask
 from flask import Flask, render_template
+from flask_restplus import Api, Resource
 import requests
 
 import google_auth_oauthlib
@@ -24,89 +25,100 @@ app.secret_key = os.getenv('G_API_SECRET')
 def root_view():
     return render_template('index.html')
 
-@app.route('/profile')
-def profile():
-    if 'credentials' not in flask.session:
-        return flask.redirect(flask.url_for('get_authorization'))
+api = Api(app, doc=False)
+
+@api.route('/profile')
+class Profile(Resource):
+    def get(self):
+        if 'credentials' not in flask.session:
+            return flask.redirect(api.url_for(GetAuthorization))
+        
+        credentials = google.oauth2.credentials.Credentials(
+            **flask.session['credentials']
+        )
+        people_service = googleapiclient.discovery.build(
+            API_SERVICE_NAME,
+            API_VERSION,
+            credentials=credentials
+        )
+        profile = people_service.people().get(
+            resourceName='people/me', personFields='names,emailAddresses'
+        ).execute()
+        name = profile['names'][0]['displayName']
+        flask.session['credentials'] = credentials_to_dict(credentials)
+
+        # return flask.jsonify(**profile)
+        # return (f'Pawpaw profile for {name}. <a href="/revoke">Logout..</a>')
+        return {'name': name, 'revoke_url': '/revoke'}
     
-    credentials = google.oauth2.credentials.Credentials(
-        **flask.session['credentials']
-    )
-    people_service = googleapiclient.discovery.build(
-        API_SERVICE_NAME,
-        API_VERSION,
-        credentials=credentials
-    )
-    profile = people_service.people().get(
-        resourceName='people/me', personFields='names,emailAddresses'
-    ).execute()
-    name = profile['names'][0]['displayName']
-    flask.session['credentials'] = credentials_to_dict(credentials)
+@api.route('/authorize')
+class GetAuthorization(Resource):
+    def get(self):
+        # creating OAuth Flow
+        flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
+            CLIENT_SECRETS_FILE,
+            scopes=SCOPES
+        )
+        flow.redirect_uri = api.url_for(OAuthCallback, _external=True)
+        authorization_url, state = flow.authorization_url(
+            access_type='offline',
+            inlucde_granted_scopes='true'
+        )
 
-    # return flask.jsonify(**profile)
-    return (f'Pawpaw profile for {name}. <a href="/revoke">Logout..</a>')
-    
-@app.route('/authorize')
-def get_authorization():
-    # creating OAuth Flow
-    flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
-        CLIENT_SECRETS_FILE,
-        scopes=SCOPES
-    )
-    flow.redirect_uri = flask.url_for('oauth2callback', _external=True)
-    authorization_url, state = flow.authorization_url(
-        access_type='offline',
-        inlucde_granted_scopes='true'
-    )
+        flask.session['state'] = state
+        return flask.redirect(authorization_url)
 
-    flask.session['state'] = state
-    return flask.redirect(authorization_url)
+@api.route('/oauth2callback')
+class OAuthCallback(Resource):
+    def get(self):
+        state = flask.session['state']
 
-@app.route('/oauth2callback')
-def oauth2callback():
-    state = flask.session['state']
+        flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
+            CLIENT_SECRETS_FILE,
+            scopes=SCOPES,
+            state=state
+        )
+        flow.redirect_uri = api.url_for(OAuthCallback, _external=True)
+        
+        authorization_response = flask.request.url
+        flow.fetch_token(authorization_response=authorization_response)
 
-    flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
-        CLIENT_SECRETS_FILE,
-        scopes=SCOPES,
-        state=state
-    )
-    flow.redirect_uri = flask.url_for('oauth2callback', _external=True)
-    
-    authorization_response = flask.request.url
-    flow.fetch_token(authorization_response=authorization_response)
+        credentials = flow.credentials
+        flask.session['credentials'] = credentials_to_dict(credentials)
 
-    credentials = flow.credentials
-    flask.session['credentials'] = credentials_to_dict(credentials)
+        return flask.redirect(api.url_for(Profile))
 
-    return flask.redirect(flask.url_for('profile'))
+@api.route('/revoke')
+class Revoke(Resource):    
+    def get(self):
+        if 'credentials' not in flask.session:
+            return ('OAuth unavailable. Authorize first: <a href="/authorize">authorize</a>.')
+        
+        credentials = google.oauth2.credentials.Credentials(
+            **flask.session['credentials']
+        )
 
-@app.route('/revoke')
-def revoke_oauth():
-    if 'credentials' not in flask.session:
-        return ('OAuth unavailable. Authorize first: <a href="/authorize">authorize</a>.')
-    
-    credentials = google.oauth2.credentials.Credentials(
-        **flask.session['credentials']
-    )
+        revoke = requests.post(
+            'https://oauth2.googleapis.com/revoke',
+            params={'token': credentials.token},
+            headers={'content-type': 'application/x-www-form-urlencoded'}
+        )
+        if revoke.status_code == 200:
+            # also clear credentials on the way out..
+            ClearCredentials().get()
+            # return ('Credentials successfully revoked. <a href="/">Go home</a>')
+            return {'msg': 'Credentials successfully revoked.', 'next_url': '/'}
+        else:
+            # return ('An error occured.')
+            return {'msg': 'An error occured.'}
 
-    revoke = requests.post(
-        'https://oauth2.googleapis.com/revoke',
-        params={'token': credentials.token},
-        headers={'content-type': 'application/x-www-form-urlencoded'}
-    )
-    if revoke.status_code == 200:
-        # also clear credentials on the way out..
-        clear_credentials()
-        return ('Credentials successfully revoked. <a href="/">Go home</a>')
-    else:
-        return ('An error occured.')
-
-@app.route('/clear')
-def clear_credentials():
-    if 'credentials' in flask.session:
-        del flask.session['credentials']
-    return ('Credentials have been cleared. <a href="/">Go home</a>')
+@api.route('/clear')
+class ClearCredentials(Resource):
+    def get(self):
+        if 'credentials' in flask.session:
+            del flask.session['credentials']
+        # return ('Credentials have been cleared. <a href="/">Go home</a>')
+        return {'msg': 'Credentials have been cleared.', 'next_url': '/'}
 
 
 def credentials_to_dict(credentials):
